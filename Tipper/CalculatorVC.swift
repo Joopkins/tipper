89b81f86dc58930e390a2157bd14477acbb76372
Joopkins <jobndingdier@gmail.com>
2016-08-17 00:22:52 -0400

    feat: Add calculator logic

--- Tipper/CalculatorVC.swift
+++ Tipper/CalculatorVC.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class CalculatorVC: UIViewController {
 
+    // MARK: - @IBOutlets
+    @IBOutlet weak var TipperTitleView: UIView!
+    @IBOutlet weak var TipperTitleLabel: UILabel!
+    @IBOutlet weak var BillAmountTextField: UITextField!
+    @IBOutlet weak var TipPercentLabel: UILabel!
+    @IBOutlet weak var TipPercentSlider: UISlider!
+    @IBOutlet weak var TipAmountLabel: UILabel!
+    @IBOutlet weak var TotalAmountLabel: UILabel!
+    
+    // MARK: - @Properties
+    var tipCalculator = TipCalculator(billAmount: 0.0, tipPercent: 0.0)
+    
+    
+    // MARK: - Initialize Views
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: - @IBActions
+    @IBAction func billAmountChanged(_ sender: AnyObject) {
+        calculateTip()
+    }
+    
+    // MARK: - Functions
+    func calculateTip() {
+        tipCalculator.tipPercent = Double(TipPercentSlider.value)
+        if let billAmount = BillAmountTextField.text {
+            tipCalculator.billAmount = (billAmount as NSString).doubleValue
+        }
+        
+        tipCalculator.calculateTip()
+    }
 }
 
